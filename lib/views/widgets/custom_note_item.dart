d7d3aa2711818f899bb86fc03842303e688cbe8b
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2025-11-21 02:28:58 +0200

    Updated theme and added custom note item widget and

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24.0, bottom: 24.0, left: 16),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ListTile(
+            title: const Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                'Flutter tips',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            subtitle: Text(
+              'Build your creater with Tharwat samy',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black.withOpacity(.5),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: null,
+              icon: () {
+                return const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                );
+              }(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24, top: 16),
+            child: Text('May 21,2022',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                )),
+          ),
+
+          // حل اخر لو حبيت تستخدم ال Row بدل ListTile
+
+          // const Row(
+          //   children: [
+          //     Text(
+          //       'Flutter tips',
+          //       style: TextStyle(fontSize: 24.0, color: Colors.black),
+          //     ),
+          //     Spacer(),
+          //     IconButton(
+          //       onPressed: null,
+          //       icon: Icon(
+          //         Icons.delete,
+          //         color: Colors.black,
+          //         size: 40,
+          //       ),
+          //     )
+          //   ],
+          // ),
+          // Text(
+          //   'Build your creater with Tharwat samy',
+          //   style: TextStyle(
+          //       fontSize: 24.0, color: Colors.black.withOpacity(0.5)),
+          // ),
+          // Align(
+          //     alignment: Alignment.centerRight,
+          //     child: Text('May 21,2022',
+          //         style: TextStyle(color: Colors.black.withOpacity(0.5)))),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_bady.dart
+++ lib/views/widgets/notes_view_bady.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_note_item.dart';
 
 class NotesViewBady extends StatelessWidget {
   const NotesViewBady({super.key});
@@ -18,81 +19,9 @@ class NotesViewBady extends StatelessWidget {
               SizedBox(
                 height: 32,
               ),
-              NoteItem()
+              NoteItem(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter tips',
-                style: TextStyle(fontSize: 24.0, color: Colors.black),
-              ),
-              subtitle: Text(
-                'Build your creater with Tharwat samy',
-                style: TextStyle(
-                    fontSize: 18.0, color: Colors.black.withOpacity(.5)),
-              ),
-              trailing: const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                  size: 40,
-                ),
-              ),
-            ),
-            Text('May 21,2022',
-                style: TextStyle(color: Colors.black.withOpacity(0.5)))
-            // const Row(
-            //   children: [
-            //     Text(
-            //       'Flutter tips',
-            //       style: TextStyle(fontSize: 24.0, color: Colors.black),
-            //     ),
-            //     Spacer(),
-            //     IconButton(
-            //       onPressed: null,
-            //       icon: Icon(
-            //         Icons.delete,
-            //         color: Colors.black,
-            //         size: 40,
-            //       ),
-            //     )
-            //   ],
-            // ),
-            // Text(
-            //   'Build your creater with Tharwat samy',
-            //   style: TextStyle(
-            //       fontSize: 24.0, color: Colors.black.withOpacity(0.5)),
-            // ),
-            // Align(
-            //     alignment: Alignment.centerRight,
-            //     child: Text('May 21,2022',
-            //         style: TextStyle(color: Colors.black.withOpacity(0.5)))),
-          ],
         ),
       ),
     );
