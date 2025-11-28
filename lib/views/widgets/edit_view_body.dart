319e456b3e49df1d38fc6552cca9d04409d7aa8f
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2025-11-28 21:17:21 +0200

    `Added Hive for local storage and updated code accordingly`

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -48,7 +48,6 @@ class EditNoteViewbady extends StatelessWidget {
               keyboardType: TextInputType.text,
             ),
             const SizedBox(height: 32),
-            Image.asset('assets/image/logo.png'),
           ],
         ),
       ),
