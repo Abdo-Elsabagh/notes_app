c172e23f09738795b517851f7dd182b8791d524a
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2025-12-05 02:15:07 +0200

    `Added BlocProvider and MultiBlocProvider to main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constanst.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/splash_view.dart';
 
@@ -16,31 +18,38 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
-        inputDecorationTheme: InputDecorationTheme(
-          labelStyle: const TextStyle(color: kPrimaryColors),
-          suffixIconColor: kPrimaryColors,
-          prefixIconColor: kPrimaryColors,
-          hintStyle: const TextStyle(color: kPrimaryColors),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: const BorderSide(color: kPrimaryColors)),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: const BorderSide(color: Colors.white)),
-          focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: const BorderSide(color: Colors.red)),
-          errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: const BorderSide(color: Colors.red)),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          inputDecorationTheme: InputDecorationTheme(
+            labelStyle: const TextStyle(color: kPrimaryColors),
+            suffixIconColor: kPrimaryColors,
+            prefixIconColor: kPrimaryColors,
+            hintStyle: const TextStyle(color: kPrimaryColors),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: kPrimaryColors)),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Colors.white)),
+            focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Colors.red)),
+            errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(color: Colors.red)),
+          ),
         ),
+        home: const SplachView(),
       ),
-      home: const SplachView(),
     );
   }
 }
